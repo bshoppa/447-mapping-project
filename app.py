ccfa33a21ebf448def0ded9a7e6eda936eba03fa
Bailey Shoppa <bshoppa1@umbc.edu>
2021-04-05 15:24:48 -0400

    got flask server working - added facility json download data - added image host

--- app.py
+++ app.py
@@ -1,21 +1,54 @@
 from flask import Flask
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, send_file
 
-# Very useful when determining current date
 import datetime
+import csv
 
 app = Flask(__name__)
 
 notes = []
 
-@app.route('/<name>', methods=['POST','GET'])
+facilities = {}
+
+with open("CA-historical-data.csv") as csvfile:
+    reader = csv.DictReader(csvfile, skipinitialspace=True)
+    for row in reader:
+        # print(dict(row))
+        facilities[row['Facility.ID']] = {
+            'Latitude': row['Latitude'],
+            'Longitude': row['Longitude'],
+            'Name': row['Name'],
+            'Cases': row['Residents.Confirmed']
+        }
+
+print("Data loaded.")
+
+@app.route('/date', methods=['POST','GET'])
+def get_prison_data():
+    print("he")
+    if request.method == 'POST':
+        return facilities
+
+@app.route('/myname<name>', methods=['POST','GET'])
 def test_page(name):
+    print("forming response")
     name = name.capitalize()
     string = "Hello " + name
     return render_template("test.html", name=string)
 
+@app.route('/public/icon1.png')
+def get_icon():
+    return send_file("public/icon1.png", mimetype = 'image/png')
+@app.route('/public/icon2.png')
+def get_icon2():
+    return send_file("public/icon2.png", mimetype = 'image/png')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
+    return render_template("index.html")
+
     headline = "Welcome Newcomer!"
     if request.method == "POST":
         if len(notes) > 0:
@@ -27,9 +60,9 @@ def index():
     # Way to include the dates into the html in a selectable way
     dates = [101021,101121,101221,101321,101421,101521]
     # pass in headline variable to html
-    return render_template("index.html", headline=headline,dates=dates)
 
-# Huge Security Violation
+'''# Huge Security Violation
 # Remove debug when fully deployed
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
+'''
