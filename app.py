fbf5b2e883c5631820754037b206a68cb2685708
Bailey Shoppa <bshoppa1@umbc.edu>
2021-04-07 15:02:21 -0400

    - moved webpage script to public/display.js - "fixed" the test_page template thing

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ facilities = {}
 with open("CA-historical-data.csv") as csvfile:
     reader = csv.DictReader(csvfile, skipinitialspace=True)
     for row in reader:
-        # print(dict(row))
         facilities[row['Facility.ID']] = {
             'Latitude': row['Latitude'],
             'Longitude': row['Longitude'],
@@ -21,26 +20,14 @@ with open("CA-historical-data.csv") as csvfile:
             'Cases': row['Residents.Confirmed']
         }
 
-print("Data loaded.")
-
 @app.route('/date', methods=['POST','GET'])
 def get_prison_data():
-    print("he")
     if request.method == 'POST':
         return facilities
 
-def test_index_message():
-    headline = "Welcome Newcomer!"
-    if request.method == "POST":
-        if len(notes) > 0:
-            del notes[0]
-        note = request.form.get("note")
-        notes.append(note)
-        return redirect(f'\{notes[0]}') # this line returns an error
-    # Way to include the dates into the html in a selectable way
-    dates = [101021,101121,101221,101321,101421,101521]
-    # pass in headline variable to html
-
+@app.route('/public/<path:path>')
+def send_public(path):
+    return send_from_directory('public', path)
 
 @app.route('/myname<name>', methods=['POST','GET'])
 def test_page(name):
@@ -49,17 +36,21 @@ def test_page(name):
     string = "Hello " + name
     return render_template("test.html", name=string)
 
-@app.route('/public/icon1.png')
-def get_icon():
-    return send_file("public/icon1.png", mimetype = 'image/png')
-@app.route('/public/icon2.png')
-def get_icon2():
-    return send_file("public/icon2.png", mimetype = 'image/png')
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    headline = "Welcome Newcomer!"
+    if request.method == "POST":
+        if len(notes) > 0:
+            del notes[0]
+        note = request.form.get("note")
+        notes.append(note)
+        print(note)
+        return redirect(f'date={notes[0]}') # this line returns an error
+    # Way to include the dates into the html in a selectable way
+    dates = [101021,101121,101221,101321,101421,101521]
+    # pass in headline variable to html
 
 
 '''# Huge Security Violation
---
+++ public/display.js
@@ -0,0 +1,82 @@
+var mymap = L.map('mapid').setView([37.125286284966805, -120.0146484375], 6);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiNDQ3dGVhbTEwIiwiYSI6ImNrbjQyZTR5ZzFuanAydmxudDBsdG5qeHQifQ.ZRPB_88o-9nBmNgmKBw50Q', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	id: 'mapbox/streets-v11',
+	tileSize: 512,
+	zoomOffset: -1
+}).addTo(mymap);
+
+/** onClick Listener, zooms in on map when user clicks on location**/
+function onMapClick(e) {
+		 mymap.zoomIn();
+}
+mymap.on('click', onMapClick);
+
+/** Color Settings that will coordinate with Covid Cases Density **/
+function getColor(d) {
+return d > 1000 ? '#800026' :
+	   d > 500  ? '#BD0026' :
+	   d > 200  ? '#E31A1C' :
+	   d > 100  ? '#FC4E2A' :
+	   d > 50   ? '#FD8D3C' :
+	   d > 20   ? '#FEB24C' :
+	   d > 10   ? '#FED976' :
+				  '#FFEDA0';
+}
+
+/** styling function for our GeoJSON layer so that its fillColor depends on our covid cases database **/
+function style(feature) {
+return {
+	fillColor: getColor(feature.properties.density),
+	weight: 2,
+	opacity: 1,
+	color: 'white',
+	dashArray: '3',
+	fillOpacity: 0.7
+};
+}
+
+/** L.geoJson(statesData, {style: style}).addTo(map); **/
+
+// load every prison
+fetch('/date',
+{"method": "POST", 'headers': {'Accept': 'application/json', 'Content-Type': 'application/json'}, "body" : JSON.stringify({})}
+).then(res => res.json()).then(data => {
+console.log(data);
+for(var key in data){
+	var lva = data[key]
+	if(lva.Latitude != "NA" && Number(lva.Cases) > 0){
+		console.log(lva.Latitude, lva.Longitude);
+					var mycircle = L.circle([Number(lva.Latitude), Number(lva.Longitude)], {
+							color: '#0033FF',
+						fillOpacity: 0.5,
+						radius: 500
+					}).addTo(mymap).bindPopup(Number(lva.Cases).toString() + ' Cases');
+
+					if(Number(lva.Cases) > 1000){
+							mycircle = L.circle([Number(lva.Latitude), Number(lva.Longitude)], {
+							color: '#FF0033',
+						fillOpacity: 0.5,
+						radius: 500
+						}).addTo(mymap).bindPopup(Number(lva.Cases).toString() + ' Cases');
+					}
+					/**
+		var myIcon = L.icon({
+			iconUrl: "public/icon1.png"
+		});
+
+		if(Number(lva.Cases) > 1000){
+			myIcon = L.icon({
+				iconUrl: "public/icon2.png"
+			});
+
+		}
+					**/
+		console.log(Number(lva.Latitude));
+		/**var marker = L.marker([Number(lva.Latitude), Number(lva.Longitude)], {icon: myIcon}).addTo(mymap); **/
+	}
+}
+});
