f9bbf914b12ab793b82439c090840ca82cbd1594
Skylar Kurtz <skurtz1@umbc.edu>
2021-04-12 16:16:35 -0400

    Readded the get_prison_data(date) function as it was removed while people were commiting a bunch

--- app.py
+++ app.py
@@ -48,13 +48,18 @@ for key in facilities:
             print("nothing happened!!!!")
 
 # set up website urls
-@app.route('/date', methods=['POST','GET'])
-def get_prison_data():
-    print("he")
+@app.route('/date/<date>', methods=['POST','GET'])
+def get_prison_data(date):
+    data = []
+    for facility in facilities.items():
+        if facility[1].get("Date") <= date:
+            data.append(facility)
+
     if request.method == 'POST':
         return facilities
-
-
+        return "Oops all berries, should not be here!"
+    else:
+        return render_template("dates.html", dates=data)
 
 @app.route('/counties', methods=['POST','GET'])
 def send_counties():
@@ -78,16 +83,12 @@ def index():
         return render_template("index.html")
 
     if request.method == "POST":
-        #if len(notes) > 0:
-        #    del notes[0]
         day = request.form.get("day")
         month = request.form.get('month')
         year = request.form.get('year')
         #if(int(day) > 31 or int(day) < 0):
 
         date = year + "-" + month + "-" + day
-        #notes.append(notes)
-        #print(notes)
         return redirect(f'/date/{date}')
 
     # Way to include the dates into the html in a selectable way
